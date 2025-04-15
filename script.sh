abc5a90eb409ac3e59e959b7b831e3996c8cce0d
Jan Äärelä <132766267+Jan-Aarela@users.noreply.github.com>
2025-04-15 06:40:37 +0300

    Update README.md

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo -e "\n\e[0;34m About ───────────────────────────────────────────────────────────────────"
+echo -e "\e[0m Electronics and Communications Engineering student at University of Oulu."
+
+echo -e "\n\e[0;34m Contact ─────────────────────────────────────────────────────────────────"
+echo -e "\e[0m Email: \e[0;30m.......................................... \e[0mjan.suomi.fin@gmail.com"
+echo -e "\e[0m LinkedIn: \e[0;30m.................................................... \e[0mjan-äärelä"
